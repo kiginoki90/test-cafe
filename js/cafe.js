4c37593f1044fbd9263ac91a6a262864afec1ae6
kiginoki90 <k1_7zk@yahoo.co.jp>
2022-09-29 15:18:10 +0900

    トップへ戻るボタンの実装

--- js/cafe.js
+++ js/cafe.js
@@ -1,20 +1,41 @@
 const backBtn = document.getElementById('back-btn');
 
-window.addEventListener('scroll',() =>{
-
-
+/**window.addEventListener('scroll',() =>{
 
 const scrollValue = document.scrollingElement.scrollTop;
 
 if(scrollValue >= 300){
-  backBtn.style.display = 'inline';
+  backBtn.style.opacity = '1';
 }
-
 else{
-  backBtn.style.display = 'none';
+  backBtn.style.opacity = '0';
 }
-});
+});**/
+
 
 $(".openbtn1").click(function () {
   $(this).toggleClass('active');
 });
+
+
+backBtn.addEventListener( 'click' , scroll_to_top );
+
+function scroll_to_top(){
+	window.scroll({top: 0, behavior: 'smooth'});
+};
+
+
+
+window.addEventListener( 'scroll' , scroll_event );
+
+function scroll_event(){
+	
+	if(window.pageYOffset > 300){
+		backBtn.style.opacity = '1';
+
+	}else	if(window.pageYOffset < 300){
+		backBtn.style.opacity = '0';
+   
+	}
+	
+};
